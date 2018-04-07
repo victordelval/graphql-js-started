f392142e1f59948268288b8099dcb4b8fa48b58b
victordelval <victor.val.andres@gmail.com>
2018-04-07 16:12:07 +0200

    Adds new dummy field to initial root query

--- schema/main.js
+++ schema/main.js
@@ -6,12 +6,31 @@ const {
     GraphQLList
 } = require('graphql');
 
+const roll = () => Math.floor(6 * Math.random()) + 1;
+
 const queryType = new GraphQLObjectType({
     name: 'RootQuery',
     fields: {
         hello: {
             type: GraphQLString,
-            resolve: () => 'world'
+            resolve: () => 'world!'
+        },
+        diceRoll: {
+            type: new GraphQLList(GraphQLInt),
+            args: {
+                count: {
+                    type: GraphQLInt,
+                    defaultValue: 2
+                }
+            },
+            // resolve: () => [roll(), roll()]
+            resolve: (_, args) => {
+                let rolls = [];
+                for (let i = 0; i < args.count; i++) {
+                    rolls.push(roll());
+                }
+                return rolls;
+            }
         }
     }
 });
